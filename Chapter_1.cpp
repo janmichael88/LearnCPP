d42b66b22a6f7ba08525de74c7b81866871bcee9
Jan Michael Cayabyab Austria <55464895+janmichael88@users.noreply.github.com>
2020-07-20 13:15:34 -0700

    Add files via upload

---
+++ Chapter_1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main(){
+
+std::cout << "Enter a Number:";
+int num1 = 0;
+std::cin >> num1;
+std::cout << "Enter a second number:";
+int num2 = 0;
+std::cin >> num2;
+std::cout  << num1 << "+" << num2 << "is" << num1+num2 << '\n';
+std::cout  << num1 << "-" << num2 << "is" << num1-num2 << '\n';
+return 0;
+
+}
+
